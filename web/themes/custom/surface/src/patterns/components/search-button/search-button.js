569414df7aaf68c2cd735afed3da451dffdd5f97
Chaz Chumley <chaz.chumley@gmail.com>
2022-12-15 20:06:19 -0500

    cleanup search js to only trigger once

--- web/themes/custom/surface/src/patterns/components/search-button/search-button.js
+++ web/themes/custom/surface/src/patterns/components/search-button/search-button.js
@@ -1,70 +1,87 @@
 (function (Drupal) {
+  const searchType = document.getElementsByTagName('body')[0].getAttribute('data-search');
   const searchButton = document.querySelector('[data-drupal-selector="search-button"]');
   const searchContainer = document.querySelector('[data-drupal-selector="site-search"]');
-  const searchInput = document.querySelector('[data-drupal-selector="edit-keys"]');
+  const searchInput = searchContainer.querySelector('input[type="search"]');
 
   function searchIsVisible() {
-    if(!searchContainer) return false;
     return searchContainer.classList.contains('is-active');
+  }
+
+  function watchForClickOut(e) {
+    let clickInSearchArea = e.target.classList.contains('form-element--type-search');
+    if (!clickInSearchArea && searchIsVisible()) {
+      toggleSearchVisibility(false);
+    }
+  }
+
+  function watchForFocusOut(e) {
+    if (e.relatedTarget) {
+      let inSearchBar = e.relatedTarget.classList.contains('form-element--type-search');
+      let inSearchButton = e.relatedTarget.classList.contains('search-block-form__submit');
+      if (!inSearchBar && !inSearchButton) {
+        toggleSearchVisibility(false);
+      }
+    }
+  }
+
+  function watchForEscapeOut(e) {
+    if (e.key === 'Escape' || e.key === 'Esc') {
+      toggleSearchVisibility(false);
+    }
   }
 
   function handleFocus() {
     if (searchIsVisible()) {
-      searchContainer.querySelector('input[type="search"]').focus();
+      searchInput.focus();
+    } else if (searchContainer.contains(document.activeElement)) {
+      searchButton.focus();
     }
   }
 
-  function toggleSearch() {
-    if (searchIsVisible()) {
-      closeSearch();
-    } else {
-      openSearch();
-    }
-  }
-
-  function openSearch() {
-    if (searchContainer) {
-      searchButton.setAttribute('aria-expanded', 'true');
-      searchContainer.classList.add('is-active');
-      searchContainer.addEventListener('transitionend', handleFocus, {
-        once: true
-      });
-    }
-  }
-
-  function closeSearch() {
-    searchButton.setAttribute('aria-expanded', 'false');
-    searchContainer.classList.remove('is-active');
-    searchButton.focus();
-  }
-
-  if(searchContainer) {
-    searchContainer.addEventListener('focusout', function (e) {
-      closeSearch();
+  function toggleSearchVisibility(visibility) {
+    searchButton.setAttribute('aria-expanded', visibility === true);
+    searchContainer.addEventListener('transitionend', handleFocus, {
+      once: true
     });
+
+    if (visibility === true) {
+      searchContainer.classList.add('is-active');
+      document.addEventListener('click', watchForClickOut, {
+        capture: true
+      });
+      document.addEventListener('focusout', watchForFocusOut, {
+        capture: true
+      });
+      document.addEventListener('keyup', watchForEscapeOut, {
+        capture: true
+      });
+
+      console.log(searchIsVisible());
+    } else {
+      searchContainer.classList.remove('is-active');
+      document.removeEventListener('click', watchForClickOut, {
+        capture: true
+      });
+      document.removeEventListener('focusout', watchForFocusOut, {
+        capture: true
+      });
+      document.removeEventListener('keyup', watchForEscapeOut, {
+        capture: true
+      });
+
+      console.log(searchIsVisible());
+    }
   }
 
   Drupal.behaviors.search = {
     attach: function attach(context) {
-      var searchButton = once('site-search', '[data-drupal-selector="search-button"]', context).shift();
+      let searchButtonEl = once('search-button', searchButton, context).shift();
 
-      if(searchButton) {
-        searchButton.addEventListener('mousedown', function () {
-          toggleSearch();
-        });
-
-        searchButton.addEventListener('keyup', function (e) {
-          if (e.key === ' ' || e.code === 'Space') {
-            openSearch();
-          }
-        });
-      }
-
-      if(searchInput) {
-        searchInput.addEventListener('keyup', function (e) {
-          if (e.key === 'Escape' || e.code === 'Escape') {
-            closeSearch();
-          }
+      if(searchButtonEl) {
+        searchButtonEl.setAttribute('aria-expanded', searchIsVisible());
+        searchButtonEl.addEventListener('click', function () {
+          toggleSearchVisibility(!searchIsVisible());
         });
       }
     }
